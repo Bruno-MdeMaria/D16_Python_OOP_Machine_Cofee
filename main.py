f7208328bb164311388d12fdf8e9e55438f335c3
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-08-17 02:04:16 -0300

    simplificacao de ifs

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 dinheiro_maquina = MoneyMachine()
-dinheiro_maquina.report()
+#dinheiro_maquina.report()   TEMP
 coffee_maker = CoffeeMaker()
-coffee_maker.report()
+#coffee_maker.report()   TEMP
 menu = Menu()
 
 maquina_on = True
@@ -20,12 +20,17 @@ while maquina_on:
         coffee_maker.report()
     else:
         bebida = menu.find_drink(escolha)
-        print(bebida)
+        #print(bebida) #TEMP
         recurso = coffee_maker.is_resource_sufficient(bebida)
-        print(recurso)
+        #print(recurso) #TEMP
         if recurso == True:
             if dinheiro_maquina.make_payment(bebida.cost):    #SE FOR VERDADEIRO TRUE O RESULTADO DA ACÃO DE VERIFCAR DINHEIRO SUFICIENTE:
                 coffee_maker.make_coffee(bebida)
+
+#OUTRA FORMA D SIMPLIFCAR O CÓDIGO É JUNTANDO AS DUAS VERIFICACOES IF:
+
+#   if recurso = coffee_maker.is_resource_sufficient(bebida) and if dinheiro_maquina.make_payment(bebida.cost):
+         # coffee_maker.make_coffee(bebida)
 
             
     
